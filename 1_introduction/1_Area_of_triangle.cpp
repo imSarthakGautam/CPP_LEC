a1ef204e3ecae2115b97b7dc0bccf18b90f290cf
imSarthakGautam <sarthakgtm175@gmail.com>
2022-05-19 12:14:01 +0545

    1_Area_of_triangle

---
+++ 1_introduction/1_Area_of_triangle.cpp
@@ -0,0 +1,17 @@
+//WAP to calculate area of triangle.
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int height ,base;
+    float area;
+    cout<<"Enter the height of triangle: ";
+    cin>>height;
+    cout<<"Enter the base of triangle: ";
+    cin>>base; 
+    area = 0.5*base*height;
+    cout<<"The area of the given triangle is : "<< area;
+    return 0;
+}
---
+++ 1_introduction/2_structure1.cpp
@@ -0,0 +1,28 @@
+//q. WAP to read the record of student's name, address and roll number in C++. Also display them using structure
+
+#include <iostream>
+
+using namespace std;
+
+struct student
+{
+    char name[10];
+    char address[15];
+    char roll[15];
+}student;
+
+
+int main(){
+    cout<<"Enter your name: ";
+    cin>>student.name;
+    cout<<"Enter your address: ";
+    cin>>student.address;
+    cout<<"Enter your Roll No : ";
+    cin>>student.roll;
+
+    cout<<"Name: "<<student.name;
+    cout<<"\n Address: "<<student.address;
+    cout<<"\n Roll No: "<<student.roll;
+    return 0;
+}
+
