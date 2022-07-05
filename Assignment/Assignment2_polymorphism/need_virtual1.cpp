ea0e9e410bd30f5534e53aa3da8de43acddb039e
imSarthakGautam <sarthakgtm175@gmail.com>
2022-07-06 05:20:41 +0545

    neecessity of a virtual function

---
+++ Assignment/Assignment2_polymorphism/need_virtual1.cpp
@@ -0,0 +1,44 @@
+// need_virtual1.cpp
+// neecessity of a virtual function
+
+#include<iostream>
+using namespace std;
+
+class Animal{
+    public:
+        void display(){
+            cout<<"\n from base class animal";
+        }
+};
+
+class Cow: public Animal{
+    public:
+        void display(){
+            cout<<"\n from derived class Cow";
+        }
+};
+
+class Dog: public Animal{
+    public:
+        void display(){
+            cout<<"\n from derived class Dog";
+        }
+};
+
+int main(){
+    Animal *panm; //pointer to base class
+    Animal anm;
+    Cow cw;
+    Dog dg;
+
+    panm=&anm;
+    panm->display();
+
+    panm=&cw;
+    panm->display();
+
+    panm=&dg;
+    panm->display();
+
+    return 0;
+}
