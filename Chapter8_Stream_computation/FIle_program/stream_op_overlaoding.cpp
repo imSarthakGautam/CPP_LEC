cf4e04cef0810b0a24cb3a6a53f594882df0d367
imSarthakGautam <sarthakgtm175@gmail.com>
2022-07-21 13:50:04 +0545

    Insertion/Extraction operator overloading

--- Chapter8_Stream_computation/FIle_program/stream_op_overlaoding.cpp
+++ Chapter8_Stream_computation/FIle_program/stream_op_overlaoding.cpp
@@ -1,8 +1,40 @@
-#include <iostream>
+
+
+#include<iostream>
 using namespace std;
-int main()
-{
-return 0;
+class student{
+    int age;
+
+    public:
+        student(){
+            age=25;
+        }
+
+        friend istream& operator >>(istream& input, student obj);
+        friend ostream& operator <<(ostream& output, student obj);
+};
+
+//cin is object of istream so returning reference of istream
+//1st param- passing the reference of istream
+//2nd param- passing the reference of ostream
+istream& operator >>(istream& input, student obj){
+    input>>obj.age;
+    return input;
+}
+
+//1st param- passing the reference of ostream
+//2nd param- object of our own class i.e. student class
+ostream& operator <<(ostream& output, student obj){
+    output<<obj.age;
+    return output;
+}
+
+int main(){
+    student s;
+    cout<<s;
+    cin>>s;
+    cout<<s; //prints 25
+    return 0;
 }
 
 // write a program
